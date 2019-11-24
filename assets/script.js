54f69da6591f1b16ed7ade65a3fec42863640be7
Natalie Carr <natticarr@gmail.com>
2019-11-24 21:39:57 +1100

    added quiz localstorage for user scores and displays past scores in quiz end function

--- assets/script.js
+++ assets/script.js
@@ -12,6 +12,8 @@ var secondsLeft = 60;
 var timeEl = document.querySelector("#time");
 var SECONDS_DEDUCTION = 10;
 var timeRanOut = 0;
+var quizFinished = 0;
+
 
 
 function quizFunction(questionNum) {
@@ -32,11 +34,14 @@ function quizFunction(questionNum) {
             }
             document.body.appendChild(alertText); //display 'Correct' or 'Wrong' after selecting answer
             //remove alert 2 seconds after being displayed
-            setTimeout(function () { alertText.parentElement.removeChild(alertText); }, 2000);
+            setTimeout(function () {
+                alertText.parentElement.removeChild(alertText);
+            }, 2000);
             questionNum++;
             buttonContainer.innerHTML = '';
             //display ending screen at end of quiz
             if (questionNum === questionsArray.length) {
+                quizFinished = 1;
                 quizEndDisplay();
             }
             quizFunction(questionNum);
@@ -46,30 +51,80 @@ function quizFunction(questionNum) {
 
 function quizEndDisplay() {
     buttonContainer.innerHTML = '';
-    if (timeRanOut===0) {
+    if (timeRanOut === 0) {
         questionText.textContent = 'You finished the quiz! You got ' + numQuestionsCorrect + ' questions correct!'
     }
     else {
         questionText.textContent = 'Time ran out! You got ' + numQuestionsCorrect + ' questions correct!'
     }
-   
+    var initialsForSave = document.createElement("input");
+    initialsForSave.setAttribute("type", "text");
+    initialsForSave.placeholder = "Type your initials here!";
+    initialsForSave.style = "margin-left: 10px";
+    var submitButton = document.createElement("button");
+    submitButton.textContent = "Submit";
+    buttonContainer.appendChild(document.createElement("p")).innerText = "Type your initials here to save your quiz score: "
+    buttonContainer.appendChild(initialsForSave);
+    buttonContainer.appendChild(submitButton);
+    submitButton.style = "margin-left: 10px";
+
+    submitButton.addEventListener("click", function (event) {
+        var user = initialsForSave.value;
+        var usernameAndScoreObject = {
+            username: user,
+            highscore: numQuestionsCorrect
+        };
+        if (localStorage.getItem("highScoresString") !== null) { //checks if there are past high scores on users PC
+            var highScoresString = localStorage.getItem("highScoresString");
+            var highScoresArray = JSON.parse(highScoresString);
+        }
+        else {
+            var highScoresArray = [];
+        }
+        highScoresArray.push(usernameAndScoreObject);
+        var newHighScoresString = JSON.stringify(highScoresArray);
+        localStorage.setItem("highScoresString", newHighScoresString);
+        pastQuizUsers();
+    });
+
+
 
 };
 
+function pastQuizUsers() {
+
+    var highScoresString = localStorage.getItem("highScoresString");
+    var highScoresArray = JSON.parse(highScoresString);
+    buttonContainer.innerHTML = '';
+    questionText.textContent = "Top High Scores";
+    var ul = document.createElement("ul");
+    buttonContainer.appendChild(ul);
+
+    for (var i = 0; i < highScoresArray.length; i++) {
+        var usernameAndScore = document.createElement("li");
+        usernameAndScore.innerHTML = "<strong>" + highScoresArray[i].username + ": </strong>  "
+            + highScoresArray[i].highscore + "";
+        usernameAndScore.style = "list-style: none";
+        ul.appendChild(usernameAndScore);
+    };
+
+
+}
+
 
 startButton.addEventListener("click", function (event) {
-
-
     var timerInterval = setInterval(function () {
         secondsLeft--;
         timeEl.textContent = secondsLeft;
-
+        if (quizFinished === 1) {
+            clearInterval(timerInterval);
+            quizEndDisplay();
+        }
         if (secondsLeft === 0) {
             clearInterval(timerInterval);
             timeRanOut = 1;
             quizEndDisplay();
         }
-
     }, 1000);
     startButton.parentElement.removeChild(startButton); //remove the start button
     quizFunction(questionNum);
