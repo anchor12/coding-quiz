1008a86196206de9acaca3b99d062aff69f961c7
Natalie Carr <natticarr@gmail.com>
2019-11-24 19:41:54 +1100

    added quiz ending display function

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-startButton = document.querySelector("#start-button");
+var startButton = document.querySelector("#start-button");
 var questionsArray = ["Which is best animal?", "What colour is sky?", "What time is the night?", "How many legs does horse have?"];
 var answerChoiceArray = [["Dog", "Cat", "Chicken", "Monkey"], ["Blue", "Green", "Pink", "Rainbow"], ["Late", "7am", "Lunchtime", "3pm"], ["4", "7", "1", "0"]]
 var correctAnswersArray = ["Dog", "Blue", "Late", "4"];
@@ -10,9 +10,12 @@ var numQuestionsCorrect = 0;
 
 var secondsLeft = 60;
 var timeEl = document.querySelector("#time");
+var SECONDS_DEDUCTION = 10;
+var timeRanOut = 0;
 
 
 function quizFunction(questionNum) {
+
     for (var i = 0; i < answerChoiceArray[questionNum].length; i++) {
         questionText.textContent = questionsArray[questionNum];
         var answerButton = document.createElement("button");
@@ -25,21 +28,37 @@ function quizFunction(questionNum) {
             }
             else {
                 alertText.textContent = 'Wrong!';
-                secondsLeft -= 10;
+                secondsLeft -= SECONDS_DEDUCTION;
             }
             document.body.appendChild(alertText); //display 'Correct' or 'Wrong' after selecting answer
             //remove alert 2 seconds after being displayed
             setTimeout(function () { alertText.parentElement.removeChild(alertText); }, 2000);
             questionNum++;
             buttonContainer.innerHTML = '';
+            //display ending screen at end of quiz
+            if (questionNum === questionsArray.length) {
+                quizEndDisplay();
+            }
             quizFunction(questionNum);
         });
     }
 };
 
+function quizEndDisplay() {
+    buttonContainer.innerHTML = '';
+    if (timeRanOut===0) {
+        questionText.textContent = 'You finished the quiz! You got ' + numQuestionsCorrect + ' questions correct!'
+    }
+    else {
+        questionText.textContent = 'Time ran out! You got ' + numQuestionsCorrect + ' questions correct!'
+    }
+   
+
+};
+
 
 startButton.addEventListener("click", function (event) {
-    
+
 
     var timerInterval = setInterval(function () {
         secondsLeft--;
@@ -47,7 +66,8 @@ startButton.addEventListener("click", function (event) {
 
         if (secondsLeft === 0) {
             clearInterval(timerInterval);
-            alert("OUT OF TIME");
+            timeRanOut = 1;
+            quizEndDisplay();
         }
 
     }, 1000);
